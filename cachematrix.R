30388b0b394b4d18a6d2c0a3a64b8a5ff86c31df
wacto <wtognetti@gmail.com>
2014-06-21 18:48:45 -0300

    cacheMatrix
    Routines working OK

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) s <<- inverse
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -12,4 +23,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  matrix <- x$get()
+  s <- solve(matrix, ...)
+  x$setinverse(s)
+  s
 }
+c=rbind(c(1, -1/4), c(-1/4, 1))
+c
+solve(c)
+cacheSolve(c)
+x=makeCacheMatrix(c)
+cacheSolve(x)
